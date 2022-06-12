af7b5f8cc2bf089a8dfffdfec40eddd9006bd5c6
ads-for-you <105886365+ads-for-you@users.noreply.github.com>
2022-06-13 00:43:20 +0300

    Add files via upload

--- Script/index.js
+++ Script/index.js
@@ -680,6 +680,18 @@
  
  /*]]>*/
 
+
+ // When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function up() {scrollFunction()};
+
+function scrollFunction () {
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+        document.getElementById("myBtn").style.display = "block";
+    } else {
+        document.getElementById("myBtn").style.display = "none";
+    }
+}
+
  //Прокрутка вверх//
 var t;
 function up() {
@@ -690,20 +702,7 @@ function up() {
   } else clearTimeout(t);
   return false;
 }
-// When the user scrolls down 20px from the top of the document, show the button
-window.onscroll = function() {scrollFunction()};
 
-function scrollFunction() {
-    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-        document.getElementById("myBtn").style.display = "block";
-    } else {
-        document.getElementById("myBtn").style.display = "none";
-    }
-}
 
-// When the user clicks on the button, scroll to the top of the document
-function topFunction() {
-    document.body.scrollTop = 0; // For Safari
-    document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
-}
+
 //    //
