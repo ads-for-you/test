0880f54fcbedb0116504fa68c64198ac1b459d5e
TooManChe <tooman.che@gmail.com>
2022-06-23 20:14:14 +0300

    ohhh

--- Script/index.js
+++ Script/index.js
@@ -1,4 +1,23 @@
 // Bar button//
+function viewDiv_all(){
+  if (document.getElementById("in").style.display = "none"){
+    document.getElementById("in").style.display = "block";
+    document.getElementById("beer_in").style.display = "none";
+    document.getElementById("gin_in").style.display = "none";
+    document.getElementById("liqueur_in").style.display = "none";
+    document.getElementById("tequila_in").style.display = "none";
+    document.getElementById("rum_in").style.display = "none";
+    document.getElementById("whiskey_in").style.display = "none";
+    document.getElementById("cognac_in").style.display = "none";
+    document.getElementById("vodka_in").style.display = "none";
+    document.getElementById("wine_in").style.display = "none";
+    document.getElementById("sparkling_in").style.display = "none";
+    document.getElementById("cocktails_in").style.display = "none";
+    document.getElementById("shot_in").style.display = "none";
+  }
+
+}
+
 function viewDiv_beer(){
   if ( document.getElementById("beer_in").style.display=="block") {
     document.getElementById("beer_in").style.display = "none";
@@ -712,4 +731,174 @@ function up() {
 }
 //    //
 
-//  Dragonscroll //
+//   //
+
+class ItcTabs {
+  constructor(target, config) {
+    const defaultConfig = {};
+    this._config = Object.assign(defaultConfig, config);
+    this._elTabs = typeof target === 'string' ? document.querySelector(target) : target;
+    this._elButtons = this._elTabs.querySelectorAll('.tabs__btn');
+    this._elPanes = this._elTabs.querySelectorAll('.tabs__pane');
+    this._eventShow = new Event('tab.itc.change');
+    this._init();
+    this._events();
+  }
+  _init() {
+    this._elTabs.setAttribute('role', 'tablist');
+    this._elButtons.forEach((el, index) => {
+      el.dataset.index = index;
+      el.setAttribute('role', 'tab');
+      this._elPanes[index].setAttribute('role', 'tabpanel');
+    });
+  }
+  show(elLinkTarget) {
+    const elPaneTarget = this._elPanes[elLinkTarget.dataset.index];
+    const elLinkActive = this._elTabs.querySelector('.tabs__btn_active');
+    const elPaneShow = this._elTabs.querySelector('.tabs__pane_show');
+    if (elLinkTarget === elLinkActive) {
+      return;
+    }
+    elLinkActive ? elLinkActive.classList.remove('tabs__btn_active') : null;
+    elPaneShow ? elPaneShow.classList.remove('tabs__pane_show') : null;
+    elLinkTarget.classList.add('tabs__btn_active');
+    elPaneTarget.classList.add('tabs__pane_show');
+    this._elTabs.dispatchEvent(this._eventShow);
+    elLinkTarget.focus();
+  }
+  showByIndex(index) {
+    const elLinkTarget = this._elButtons[index];
+    elLinkTarget ? this.show(elLinkTarget) : null;
+  };
+  _events() {
+    this._elTabs.addEventListener('click', (e) => {
+      const target = e.target.closest('.tabs__btn');
+      if (target) {
+        e.preventDefault();
+        this.show(target);
+      }
+    });
+  }
+}
+
+const tabs = document.querySelectorAll('.tabs');
+for (let i = 0, length = tabs.length; i < length; i++) {
+  new ItcTabs(tabs[i]);
+}
+
+//  //
+
+
+//++Tab1++//
+function openCity(evt, cityName) {
+  // Declare all variables
+  var i, tabcontent, tablinks;
+
+  // Get all elements with class="tabcontent" and hide them
+  tabcontent = document.getElementsByClassName("tabcontent");
+  for (i = 0; i < tabcontent.length; i++) {
+      tabcontent[i].style.display = "none";
+  }
+
+  // Get all elements with class="tablinks" and remove the class "active"
+  tablinks = document.getElementsByClassName("tablinks");
+  for (i = 0; i < tablinks.length; i++) {
+      tablinks[i].className = tablinks[i].className.replace(" active", "");
+  }
+
+  // Show the current tab, and add an "active" class to the button that opened the tab
+  document.getElementById(cityName).style.display = "block";
+  evt.currentTarget.className += " active";
+}
+// Get the element with id="defaultOpen" and click on it
+document.getElementById("defaultOpen").click();
+
+//--Tab1--//
+
+
+//++Dragscroll++//
+
+(function (root, factory) {
+  if (typeof define === 'function' && define.amd) {
+      define(['exports'], factory);
+  } else if (typeof exports !== 'undefined') {
+      factory(exports);
+  } else {
+      factory((root.dragscroll = {}));
+  }
+}(this, function (exports) {
+  var _window = window;
+  var _document = document;
+  var mousemove = 'mousemove';
+  var mouseup = 'mouseup';
+  var mousedown = 'mousedown';
+  var EventListener = 'EventListener';
+  var addEventListener = 'add'+EventListener;
+  var removeEventListener = 'remove'+EventListener;
+  var newScrollX, newScrollY;
+
+  var dragged = [];
+  var reset = function(i, el) {
+      for (i = 0; i < dragged.length;) {
+          el = dragged[i++];
+          el = el.container || el;
+          el[removeEventListener](mousedown, el.md, 0);
+          _window[removeEventListener](mouseup, el.mu, 0);
+          _window[removeEventListener](mousemove, el.mm, 0);
+      }
+
+      // cloning into array since HTMLCollection is updated dynamically
+      dragged = [].slice.call(_document.getElementsByClassName('dragscroll'));
+      for (i = 0; i < dragged.length;) {
+          (function(el, lastClientX, lastClientY, pushed, scroller, cont){
+              (cont = el.container || el)[addEventListener](
+                  mousedown,
+                  cont.md = function(e) {
+                      if (!el.hasAttribute('nochilddrag') ||
+                          _document.elementFromPoint(
+                              e.pageX, e.pageY
+                          ) == cont
+                      ) {
+                          pushed = 1;
+                          lastClientX = e.clientX;
+                          lastClientY = e.clientY;
+
+                         
+                      }
+                  }, 0
+              );
+
+              _window[addEventListener](
+                  mouseup, cont.mu = function() {pushed = 0;}, 0
+              );
+
+              _window[addEventListener](
+                  mousemove,
+                  cont.mm = function(e) {
+                      if (pushed) {
+                          (scroller = el.scroller||el).scrollLeft -=
+                              newScrollX = (- lastClientX + (lastClientX=e.clientX));
+                          scroller.scrollTop -=
+                              newScrollY = (- lastClientY + (lastClientY=e.clientY));
+                          if (el == _document.body) {
+                              (scroller = _document.documentElement).scrollLeft -= newScrollX;
+                              scroller.scrollTop -= newScrollY;
+                          }
+                      }
+                  }, 0
+              );
+           })(dragged[i++]);
+      }
+  }
+
+    
+  if (_document.readyState == 'complete') {
+      reset();
+  } else {
+      _window[addEventListener]('load', reset, 0);
+  }
+
+  exports.reset = reset;
+}));
+
+//--Dragscroll--//
